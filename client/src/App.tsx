a57767b3cca44df8447508e35024fe581add5f61
unknown <glodybulambo@gmail.com>
2022-05-27 04:07:52 +0200

    adding Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from './Pages/Home';
+import Test from './Pages/Test';
 
 function App() {
   return (
-    <div className="bg-black text-white">
-      RENTAL
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-blue-700 font-[700]'>
+        Home
+    </div>
+  )
+}
+
+export default Home
