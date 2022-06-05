472ae7f786f2f60a2a96c4b84af4db8158b1a49c
unknown <glodybulambo@gmail.com>
2022-06-05 22:42:57 +0200

    fixing some footer's bugs

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -7,15 +7,15 @@ const Footer = () => {
   return (
     <div className='bg-gradient-to-r from-[#040C18] to-[#002853] text-white px-4 lg:px-20 py-8 items-center'>
         <Newsletter />
-        <div className="my-8 grid gap-4 ">
-            <div className="flex-1">
+        <div className="mt-16 mb-8 flex flex-col md:flex-row">
+            <div className="flex-1 my-2">
                 <div className='flex items-center mb-4'>
                     <FaHome style={{fontSize:'20px',}}/>
                     <h1 className='text-lg font-[700] ml-2'>RENTAL</h1>
                 </div>
-                <p className="text-md text-gray-300 font-[100]">Our vision is to make all people the best place to live for them</p>
+                <p className="text-md text-gray-300 font-[100] md:w-3/4">Our vision is to make all people the best place to live for them</p>
             </div>
-            <div className="grid gap-2 grid-cols-2">
+            <div className="flex-1 grid gap-2 grid-cols-2 my-2">
                 <div className="flex flex-col">
                     <h3 className="font-[700] text-lg mb-4">About</h3>
                     <a href="" className="mb-2 text-gray-500 hover:text-gray-200">About us</a>
@@ -29,7 +29,7 @@ const Footer = () => {
                     <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Security</a>
                 </div>
             </div>
-            <div className="grid gap-2 grid-cols-2">
+            <div className="flex-1 grid gap-2 grid-cols-2 my-2">
                 <div className="flex flex-col">
                     <h3 className="font-[700] text-lg -4">Support</h3>
                     <a href="" className="mb-2 text-gray-500 hover:text-gray-200">FAQs</a>
@@ -47,8 +47,8 @@ const Footer = () => {
             </div>
         </div>
         <hr className="" />
-        <div className="m-8 flex flex-col items-center">
-            <div className="flex flex-col md:flex-row justify-between mb-4">
+        <div className="m-8 flex flex-col">
+            <div className="flex flex-col sm:flex-row justify-between mb-4 items-center">
                 <a href="" className="text-gray-500 hover:text-gray-200 my-2">Terms&Agreement</a>
                 <a href="" className="text-gray-500 hover:text-gray-200 my-2">Privacy Policy</a>
             </div>
