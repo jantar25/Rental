3b7524ea7f216ea922a03a40813be942ad8e1995
unknown <glodybulambo@gmail.com>
2022-06-19 01:31:54 +0200

    Db connected

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,26 @@
-console.log('Rental API')
+import express from 'express'
+const app = express();
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+
+
+dotenv.config();
+const dbUrl = process.env.MONGODB_URL
+
+if (typeof dbUrl === 'string') {
+    mongoose.connect(dbUrl)
+    .then(()=>console.log("DB Connected"))
+    .catch((err)=>console.log(err))
+}
+
+
+app.use(cors());
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("Hello, Welcom to Rental API")
+});
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("BackEnd Server is running on port 5000")
+})
