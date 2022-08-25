b3d6e030a7af7ccc1a88ec242c158d72656cb788
unknown <glodybulambo@gmail.com>
2022-08-25 06:28:22 +0200

    First page done

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import i18next from 'i18next';
 import Newsletter from './Newsletter'
 import { FaHome } from 'react-icons/fa';
 import { BsInstagram,BsFacebook,BsWhatsapp } from 'react-icons/bs';
@@ -14,33 +15,37 @@ const Footer = () => {
                     <FaHome style={{fontSize:'20px',}}/>
                     <h1 className='text-lg font-[700] ml-2'>RENTAL</h1>
                 </div>
-                <p className="text-md text-gray-300 font-[100] md:w-3/4">Our vision is to make all people the best place to live for them</p>
+                <p className="text-md text-gray-300 font-[100] md:w-3/4">{i18next.t('ourVision')as string}</p>
             </div>
             <div className="flex-1 grid gap-2 grid-cols-2 my-2">
                 <div className="flex flex-col">
-                    <h3 className="font-[700] text-lg mb-4">About</h3>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">About us</a>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Features</a>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">News&Blog</a>
-                </div>
-                <div className="flex flex-col">
-                    <h3 className="font-[700] text-lg mb-4">Company</h3>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">How we work?</a>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Capital</a>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Security</a>
-                </div>
-            </div>
-            <div className="flex-1 grid gap-2 grid-cols-2 my-2">
-                <div className="flex flex-col">
-                    <h3 className="font-[700] text-lg mb-4">Support</h3>
-                    <Link to='/admin'>
-                        <span className="mb-2 text-gray-500 hover:text-gray-200">Admin</span>
+                    <h3 className="font-[700] text-lg mb-4">{i18next.t('about')as string}</h3>
+                    <Link to='/about'>
+                        <a href="" className="text-gray-500 hover:text-gray-200">{i18next.t('aboutUs')as string}</a>
                     </Link>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Support center</a>
-                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">Contact us</a>
+                    <a href="" className="my-2 text-gray-500 hover:text-gray-200">{i18next.t('features')as string}</a>
+                    <a href="" className="text-gray-500 hover:text-gray-200">{i18next.t('news&blog')as string}</a>
+                </div>
+                <div className="flex flex-col">
+                    <h3 className="font-[700] text-lg mb-4">{i18next.t('comgagny')as string}</h3>
+                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">{i18next.t('howWeWork')as string}</a>
+                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">{i18next.t('capital')as string}</a>
+                    <a href="" className="mb-2 text-gray-500 hover:text-gray-200">{i18next.t('support')as string}</a>
+                </div>
+            </div>
+            <div className="flex-1 grid gap-2 grid-cols-2 my-2">
+                <div className="flex flex-col">
+                    <h3 className="font-[700] text-lg mb-4">{i18next.t('support')as string}</h3>
+                    <Link to='/admin'>
+                        <span className="text-gray-500 hover:text-gray-200">{i18next.t('admin')as string}</span>
+                    </Link>
+                    <a href="" className="my-2 text-gray-500 hover:text-gray-200">{i18next.t('supportCenter')as string}</a>
+                    <Link to='/contacts'>
+                        <a href="/contacts" className="text-gray-500 hover:text-gray-200">{i18next.t('callUs')as string}</a>
+                    </Link>
                 </div>
                 <div className="">
-                    <h3 className="font-[700] text-lg mb-4">Follow us</h3>
+                    <h3 className="font-[700] text-lg mb-4">{i18next.t('followUs')as string}</h3>
                     <div className=" flex gap-x-3">
                         <a href="" className="text-gray-500 hover:text-gray-200"><BsWhatsapp /></a>
                         <a href="" className="text-gray-500 hover:text-gray-200"><BsFacebook /></a>
@@ -52,10 +57,10 @@ const Footer = () => {
         <hr className="" />
         <div className="m-8 flex flex-col">
             <div className="flex flex-col sm:flex-row justify-between mb-4 items-center">
-                <a href="" className="text-gray-500 hover:text-gray-200 my-2">Terms&Agreement</a>
-                <a href="" className="text-gray-500 hover:text-gray-200 my-2">Privacy Policy</a>
+                <a href="" className="text-gray-500 hover:text-gray-200 my-2">{i18next.t('termsAgreement')as string}</a>
+                <a href="" className="text-gray-500 hover:text-gray-200 my-2">{i18next.t('privacyPolice')as string}</a>
             </div>
-            <p className="text-center text-gray-300">Ⓒ Jantar 2022, All right reserved</p>
+            <p className="text-center text-gray-300">{i18next.t('copyRight')as string}</p>
         </div>
     </div>
   )
--- client/src/Components/Newsletter.tsx
+++ client/src/Components/Newsletter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import i18next from 'i18next';
 import axios from "axios";
 import { FaHome } from 'react-icons/fa';
 
@@ -20,18 +21,18 @@ const Newsletter = () => {
   return (
         <div className="flex flex-col items-center">
             <div className="flex flex-col sm:flex-row items-center justify-center mb-4">
-                <h1 className="text-3xl font-[600] mr-4 text-center">Get timely update from</h1>
+                <h1 className="text-3xl font-[600] mr-4 text-center">{i18next.t('getUpdate')as string}</h1>
                 <div className='flex items-center'>
                     <FaHome style={{fontSize:'35px',}}/>
                     <h1 className='text-4xl font-[700] ml-2'>RENTAL</h1>
                 </div>
             </div>
-            <p className="text-gray-400 text-center">Subscribe and find super attractive prices quotes from us,Find you residence soon.</p>
+            <p className="text-gray-400 text-center">{i18next.t('getUpdateIntro')as string}</p>
             {err? <span className="text-red-800 py-2">{`*${err}*`}</span>: null}
             <form className="flex w-full sm:w-2/3 md:w-1/2 mt-2" onSubmit={HandleNewsletter} >
-                <input type="email" required className="flex-1 px-4 py-2 rounded-l w-full text-black" value={email} placeholder='Email' 
+                <input type="email" required className="flex-1 px-4 py-2 rounded-l w-full text-black" value={email} placeholder={i18next.t('emailPlaceholder')as string} 
                 onChange={(e)=>setEmail(e.target.value)} />
-                <button className=' bg-green-600 py-2 px-4 rounded-r text-sm font-[600]' type='submit'>Get Started</button>
+                <button className=' bg-green-600 py-2 px-4 rounded-r text-sm font-[600]' type='submit'>{i18next.t('getUpdateBtn')as string}</button>
             </form>
         </div>
   )
